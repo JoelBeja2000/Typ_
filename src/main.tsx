ee0e04c70b4058da569ed67f529f5cb325ab642d
Oveja <joelbeja2000@gmail.com>
2026-04-11 02:22:45 +0200

    fix: remove importmap and add diagnostic logs

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from '../App';
 import '../styles/main.css'; 
 
+console.log('OveTyp_ entry point loaded');
+
 const rootElement = document.getElementById('root');
+console.log('Root element found:', !!rootElement);
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
